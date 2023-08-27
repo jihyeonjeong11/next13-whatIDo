785cb6ca5d704ec0c4a1004320fe324726f64e77
jihyeonjeong11 <wjdwlgus11@gmail.com>
2023-08-27 12:30:05 +0900

    first commit: hook form

---
+++ src/app/form-page/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+import Image from 'next/image';
+import SignUpForm from '../components/form-page';
+
+export default function FormPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <SignUpForm />
+    </main>
+  );
+}
+
+// https://medium.com/@prithvi128717/creating-a-form-in-react-with-react-hook-form-and-next-js-13-4-5dae780daaef
+// https://articles.wesionary.team/react-hook-form-schema-validation-using-zod-80d406e22cd8
