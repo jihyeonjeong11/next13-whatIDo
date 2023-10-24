c254f81133b92cd7a242f56afcffc8c8e0aec492
JIHYEON JEONG <jihyeonjeong@JIHYEONs-Mac-mini.local>
2023-10-24 13:33:36 +0900

    for merge from latest

--- src/app/components/use-window/StyledApp.tsx
+++ src/app/components/use-window/StyledApp.tsx
@@ -24,24 +24,18 @@ const theme = {
 const StyledApp: FC = ({ children }) => {
   //const { themeName } = useSession();
   const { open, processes } = useProcesses();
-
-  // theme={themes[themeName] || themes[DEFAULT_THEME]}
-
+  console.log(processes);
   return (
     <StyleSheetManager enableVendorPrefixes>
-      <ThemeProvider theme={themes[DEFAULT_THEME]}>
-        <GlobalStyle />
-        <div>for test</div>
-        <button
-          aria-label="addComponent"
-          onClick={() => open(WINDOW_ID, { hasWindow: true, Component:() => <div>{"I'm component"}</div> })}
-        >
-          add component
-        </button>
-        <LazyMotion features={motionFeatures} strict>
-          <AppsLoader />
-        </LazyMotion>
-      </ThemeProvider>
+      {children}
+      <div>for test</div>
+      <button
+        aria-label="addComponent"
+        onClick={() => open("1", { Component: TestComp })}
+      >
+        add component
+      </button>
+      <AppsLoader />
     </StyleSheetManager>
   );
 };
