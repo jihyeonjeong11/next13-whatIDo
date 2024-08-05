6e461e7659ffc238b6aaf9c5c42a96180971e7e4
jihyeonjeong11 <wjdwlgus11@gmail.com>
2024-08-06 07:01:51 +0900

    nextjs15 with vanta

--- src/app/use-vanta-background/page.tsx
+++ src/app/use-vanta-background/page.tsx
@@ -1,12 +1,35 @@
 'use client';
 
-import React from 'react';
-import '@/app/use-phone-css/styles/phone.css';
-
+import React, { useEffect, useRef, useState } from 'react';
+import NET from 'vanta/dist/vanta.net.min';
+import * as THREE from 'three';
 export default function UseVanta() {
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const vantaRef = useRef(null);
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        NET({
+          el: vantaRef.current,
+          THREE,
+          color: 0x14b679,
+          backgroundColor: 0x15173c,
+          maxDistance: 34.0,
+        }),
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destory();
+    };
+  }, [vantaEffect]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      useVanta
+      <div ref={vantaRef}>
+        <p style={{ color: '#fff', paddingTop: '20px' }}>
+          Animated website backgrounds in a few lines of code.
+        </p>
+      </div>
     </main>
   );
 }
