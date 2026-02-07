4f833abad334492618cf1b03eb23bc093ce44759
jihyeonjeong11 <wjdwlgus11@gmail.com>
2026-02-08 02:16:23 +0900

    again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={`${inter.className} min-h-dvh`}>
         <Header />
         <div className="flex flex-1 min-h-dvh">
+          <Aside />
           <main className="flex-1 overflow-auto">{children}</main>
         </div>
       </body>
