b1acc5b10fb383e34df3789c99b3d912eb8bbbde
jihyeonjeong11 <wjdwlgus11@gmail.com>
2026-02-22 02:01:40 +0900

    refactor:graphview performance

--- src/app/_components/graphView/lazy.tsx
+++ src/app/_components/graphView/lazy.tsx
@@ -1,3 +1,6 @@
+'use client';
 import dynamic from 'next/dynamic';
 
-export const GraphView = dynamic(() => import('./GraphView').then((res) => res.GraphView));
+export const GraphView = dynamic(() => import('./GraphView').then((res) => res.GraphView), {
+  ssr: false,
+});
