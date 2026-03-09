63a8a9204f9a3d17d0f406266bca91aeebb2b259
jihyeonjeong11 <wjdwlgus11@gmail.com>
2026-03-09 23:57:33 +0900

    feat: 메인화면 generated 이미지 추가

--- src/app/_components/sections/posts-section.tsx
+++ src/app/_components/sections/posts-section.tsx
@@ -10,7 +10,8 @@ import {
 } from '@/components/ui/card';
 import { Container } from '@/components/ui/container';
 import { Typography } from '@/components/ui/typography';
-import { source } from '@/libs/fumadocs/source';
+import { source, getPageImage } from '@/libs/fumadocs/source';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export const PostsSection = () => {
@@ -26,31 +27,44 @@ export const PostsSection = () => {
     })
     .filter((p) => p.data.description && !p.data.title.includes('outdated'));
 
+  console.log(posts);
+
   return (
     <Container id="posts">
       <Typography variant="h2">Posts</Typography>
-      {posts.map(({ data: { title, description, tags }, url }) => (
-        <Card className="flex flex-col gap-4" key={title}>
-          <CardHeader className="flex flex-col gap-4">
-            <li className="flex">
-              {tags?.map((t) => (
-                <Badge variant="secondary" key={t}>
-                  {t}
-                </Badge>
-              ))}
-            </li>
-            <CardTitle>{title}</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <CardDescription>{description}</CardDescription>
-          </CardContent>
-          <CardFooter>
-            <Link href={`${url}`} className={buttonVariants({ variant: 'secondary' })}>
-              Read More
-            </Link>
-          </CardFooter>
-        </Card>
-      ))}
+      {posts.map((page) => {
+        const { title, description, tags } = page.data;
+        const { url } = page;
+        const ogImageUrl = `/${getPageImage(page).url}`;
+
+        return (
+          <Card className="flex flex-row gap-4" key={title}>
+            <div className="flex flex-col flex-1">
+              <CardHeader className="flex flex-col gap-4">
+                <li className="flex">
+                  {tags?.map((t) => (
+                    <Badge variant="secondary" key={t}>
+                      {t}
+                    </Badge>
+                  ))}
+                </li>
+                <CardTitle>{title}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <CardDescription>{description}</CardDescription>
+              </CardContent>
+              <CardFooter>
+                <Link href={`${url}`} className={buttonVariants({ variant: 'secondary' })}>
+                  Read More
+                </Link>
+              </CardFooter>
+            </div>
+            <div className="relative w-48 shrink-0 overflow-hidden rounded-r-lg">
+              <Image src={ogImageUrl} alt={title} fill className="object-contain" />
+            </div>
+          </Card>
+        );
+      })}
     </Container>
   );
 };
